3705e88c71067c8a9d98ef4a25e081845296a065
G485072024 <248507@st.takushoku-u.ac.jp>
2025-05-24 21:31:50 +0900

    project

--- project/gourmet.js
+++ project/gourmet.js
@@ -1,21 +1,90 @@
 
 // 課題3-2 のプログラムはこの関数の中に記述すること
 function print(data) {
-  console.log(data.results.shop[0].access)
+  for (let i = 0; i < 2; i++) {
+    let shop = data.results.shop[i];
+
+    console.log("ーーーーーーーーーーーーーーーー");
+    console.log("【店舗名】" + shop.name);
+    console.log("【ジャンル】" + shop.genre.name);
+    console.log("【サブジャンル】" + shop.sub_genre.name);
+    console.log("【キャッチコピー】" + shop.catch);
+    console.log("【アクセス】" + shop.access);
+    console.log("【住所】" + shop.address);
+    console.log("【予算】" + shop.budget.name);
+    console.log("【営業時間】" + shop.open);
+    console.log("【最寄駅】" + shop.station_name);
+  }
 }
 
 let b = document.querySelector('button#print') 
   b.addEventListener('click',greeting)
 
   function greeting(){
-    let i = document.querySelector('input[name="shimei"]') 
-    let p = document.querySelector('p#message')
-    p.textContent = '2件ヒットしました';
+    
+      let p = document.querySelector('p#message');
+      p.textContent = '2件ヒットしました';
+  
+      // 表示用のコンテナを作る（例：div）
+      let container = document.createElement('div');
+    function printDom(){
+      for (let i = 0; i < 2; i++) {
+          let shop = data.results.shop[i];
+  
+          // 店舗名
+          let h2_tenpo = document.createElement('h2');
+          h2_tenpo.textContent = "【店舗名】" + shop.name;
+          container.appendChild(h2_tenpo);
+  
+          // ジャンル
+          let h4_genre = document.createElement('h4');
+          h4_genre.textContent = "【ジャンル】" + shop.genre.name;
+          container.appendChild(h4_genre);
+  
+          // サブジャンル
+          let p_subgenre = document.createElement('p');
+          p_subgenre.textContent = "【サブジャンル】" + shop.sub_genre.name;
+          container.appendChild(p_subgenre);
+  
+          // キャッチコピー
+          let h3 = document.createElement('h3');
+          h3.textContent = "【キャッチコピー】" + shop.catch;
+          container.appendChild(h3);
+  
+          // アクセス
+          let p_access = document.createElement('p');
+          p_access.textContent = "【アクセス】" + shop.access;
+          container.appendChild(p_access);
+  
+          // 住所
+          let p_address = document.createElement('p');
+          p_address.textContent = "【住所】" + shop.address;
+          container.appendChild(p_address);
+  
+          // 予算
+          let h3_budget = document.createElement('h3');
+          h3_budget.textContent = "【予算】" + shop.budget.name;
+          container.appendChild(h3_budget);
+  
+          // 営業時間
+          let p_open = document.createElement('p');
+          p_open.textContent = "【営業時間】" + shop.open;
+          container.appendChild(p_open);
+  
+          // 最寄駅
+          let p_station = document.createElement('p');
+          p_station.textContent = "【最寄駅】" + shop.station_name;
+          container.appendChild(p_station);
+  
+          // 区切り線
+          let hr = document.createElement('hr');
+          container.appendChild(hr);
+      }
+    }
+      // 既存のp要素の後ろに表示
+      p.insertAdjacentElement('afterend', container);
   }
-// 課題5-1 の関数 printDom() はここに記述すること
-function printDom(data) {
-
-}
+  
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
 
