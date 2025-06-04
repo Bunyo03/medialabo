15ac376893e343c1334cf54e29414bef6b448af6
G485072024 <248507@st.takushoku-u.ac.jp>
2025-06-04 22:06:14 +0900

    project

--- project/gourmet.js
+++ project/gourmet.js
@@ -24,9 +24,9 @@ b.addEventListener('click', sendRequest);
   function printDom(data){
 
     let s = document.querySelector('select#omise');
-	let idx = s.selectedIndex;	// idx 番目の option が選択された
+	let idx = s.selectedIndex
 
-	let os = s.querySelectorAll('option');	// s の子要素 option をすべて検索
+	let os = s.querySelectorAll('option')
 	let o = os.item(idx);
 
 
@@ -48,62 +48,62 @@ b.addEventListener('click', sendRequest);
 
           let shop = data.results.shop[i];
   
-          // 店舗名
           let h2_tenpo = document.createElement('h2');
           h2_tenpo.textContent = "【店舗名】" + shop.name;
           container.appendChild(h2_tenpo);
   
-          // ジャンル
           let h4_genre = document.createElement('h4');
           h4_genre.textContent = "【ジャンル】" + shop.genre.name;
           container.appendChild(h4_genre);
   
-          // サブジャンル
           let p_subgenre = document.createElement('p');
           p_subgenre.textContent = "【サブジャンル】" + shop.sub_genre.name;
           container.appendChild(p_subgenre);
   
-          // キャッチコピー
           let h3 = document.createElement('h3');
           h3.textContent = "【キャッチコピー】" + shop.catch;
           container.appendChild(h3);
   
-          // アクセス
           let p_access = document.createElement('p');
           p_access.textContent = "【アクセス】" + shop.access;
           container.appendChild(p_access);
   
-          // 住所
           let p_address = document.createElement('p');
           p_address.textContent = "【住所】" + shop.address;
           container.appendChild(p_address);
-  
-          // 予算
+
           let h3_budget = document.createElement('h3');
           h3_budget.textContent = "【予算】" + shop.budget.name;
           container.appendChild(h3_budget);
-  
-          // 営業時間
+
           let p_open = document.createElement('p');
           p_open.textContent = "【営業時間】" + shop.open;
           container.appendChild(p_open);
-  
-          // 最寄駅
+
           let p_station = document.createElement('p');
           p_station.textContent = "【最寄駅】" + shop.station_name;
           container.appendChild(p_station);
     }
-      // pの後ろに表示
+      
       p.insertAdjacentElement('afterend', container);
   }
-  
-
-// 課題6-1 のイベントハンドラ登録処理は以下に記述
-// URL を設定
-// 通信開始
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+
+  let s = document.querySelector('select#omise');
+  let p = document.querySelector('p#message');
+
+  let reset = document.querySelector('div#result');
+  if (reset) {
+    reset.remove();
+  }
+
+  if (s.value !== 'izakaya') {
+    p.textContent = '「選択肢」を選んでください。';
+    return
+  }
+
 
   let url = 'https://www.nishita-lab.org/web-contents/jsons/hotpepper/G001.json'
 
@@ -115,10 +115,8 @@ function sendRequest() {
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
-// サーバから送られてきたデータを出力
 let data = resp.data;
 
-// data が文字列型なら，オブジェクトに変換する
 if (typeof data === 'string') {
   data = JSON.parse(data);
 }
@@ -136,8 +134,3 @@ function showError(err) {
 function finish() {
     console.log('Ajax 通信が終わりました');
 }
-
-////////////////////////////////////////
-// 以下はグルメのデータサンプル
-// 注意: 第5回までは以下を変更しないこと！
-// 注意2: 課題6-1 で以下をすべて削除すること
